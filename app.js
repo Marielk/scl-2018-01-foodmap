f465c1eaf024efbab5c473a685864fb885af949b
Mariel <marielk.mya@gmail.com>
2018-07-25 19:42:22 -0400

    creando intento de require

--- app.js
+++ app.js
@@ -15,5 +15,66 @@ function showPosition(position) {
     "<br>Longitude: " + position.coords.longitude; 
 }
 
+function guardarInput(){
+	let userSearch = document.getElementById('search_input').value; 
+	console.log(userSearch);   
+}
+
+const curl = new (require( 'curl-request' ))();
+
+curl.setHeaders([
+    'curl -X GET --header "Accept: application/json" --header "user-key: f7f7a4a4a441facbf11975a77711a113" "https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999"'
+])
+.get('https://developers.zomato.com/api/v2.1/categories')
+.then(response => response.json())
+.then(data => { 
+		console.log(data);
+		renderInfo(data);
+		})
+
+.catch((e) => {
+    console.log(e);
+});
+
+function buscarRestaurant(){
+		
+	curl.setHeaders([
+			'curl -X GET --header "Accept: application/json" --header "user-key: f7f7a4a4a441facbf11975a77711a113" "https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999"'
+	])
+		.get('https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999')
+			.then(response => response.json())
+				.then(data => { 
+						console.log(data);
+						renderInfo(data);
+						})
+
+			.catch((e) => {
+				console.log(e);
+			});
+
+}	
+
+		const renderInfo = (data) => {
+		containerTitle.innerHTML = data.Title;
+		containerYear.innerHTML = data.Year;
+		containerRuntime.innerHTML = data.Runtime;
+		containerImage.innerHTML = `<img src=”${data.Poster}”>`;
+		}
 
 
+
+function mostrarResultado(){
+
+}
+
+function buscarCercanos(){
+
+}
+
+function mostrarCercanos(){
+
+}
+
+function mostrarMapa(){
+	
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+requirejs.config({
+  baseUrl: 'js', 
+  paths: {
+     curl: './npm/node_modules/node-libcurl/lib/curl'   
+  }
+});
