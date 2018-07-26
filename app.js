d137d70580c027d45c56ac493d9487ce791b294c
Mariel <marielk.mya@gmail.com>
2018-07-26 11:19:50 -0400

    usando node zomato y bajando json de la API, navegando en la info entregada por la API

--- app.js
+++ app.js
@@ -1,54 +1,100 @@
+
+
 let map;
 let infowindow;
-mostrarMapa()
-var parrafo = document.getElementById("demo");
 
+llamarData();
+/*
+var parrafo = document.getElementById("demo");x
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(mostrarMapa);
     } else {
         parrafo.innerHTML = "Geolocation is not supported by this browser.";
     }
-}
+} */
 
 function guardarInput(){
 	let userSearch = document.getElementById('search_input').value; 
-	console.log(userSearch);   
+	return userSearch;   
 }
 
-const curl = new (require( 'curl-request' ))();
+function llamarData(){
+	const santiagoJSON = 'data/santiago.json';
+	fetch(santiagoJSON) 
+	.then(response => response.json())
+	.then(data => { 
+		
+		let fullData = Object.entries(data);
+		let info = fullData[0];
+		let subArr = Object.entries(info[1]); 
 
-curl.setHeaders([
-    'curl -X GET --header "Accept: application/json" --header "user-key: f7f7a4a4a441facbf11975a77711a113" "https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999"'
-])
-.get('https://developers.zomato.com/api/v2.1/categories')
-.then(response => response.json())
-.then(data => { 
-		console.log(data);
-		renderInfo(data);
-		})
-
-.catch((e) => {
-    console.log(e);
-});
+		console.log(subArr[0][1].id);
+	})
+}
 
 function buscarRestaurant(){
+	const zomato = require('zomato');
+	const client = zomato.createClient({
+		userKey: 'f7f7a4a4a441facbf11975a77711a113', //as obtained from [Zomato API](https://developers.zomato.com/apis)
+	});
+	client.getCategories(null, function(err, result){
+    if(!err){
+      console.log(result);
+    }else {
+      console.log(err);
+    }
+	});
 		
-	curl.setHeaders([
-			'curl -X GET --header "Accept: application/json" --header "user-key: f7f7a4a4a441facbf11975a77711a113" "https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999"'
-	])
-		.get('https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999')
-			.then(response => response.json())
-				.then(data => { 
-						console.log(data);
-						renderInfo(data);
-						})
+	client.getCities({
+		q: "santiago", //query by city name
+		lat:"-33.4171066", //latitude
+		lon:"-70.6395131", //longitude
+		city_ids:"1,2,3", //comma separated city_ids value
+		count:"2" // number of maximum result to display
+		}, function(err, result){
+				if(!err){
+					console.log(result);
+				}else {
+					console.log(err);
+				}
+		});
+	
+}
 
-			.catch((e) => {
-				console.log(e);
-			});
 
-}	
+// curl.setHeaders([
+//     'curl -X GET --header "Accept: application/json" --header "user-key: f7f7a4a4a441facbf11975a77711a113" "https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999"'
+// ])
+// .get('https://developers.zomato.com/api/v2.1/categories')
+// .then(response => response.json())
+// .then(data => { 
+// 		console.log(data);
+// 		renderInfo(data);
+// 		})
+
+// .catch((e) => {
+//     console.log(e);
+// });
+
+
+// function buscarRestaurant(){
+// 	const curl = require( 'node-libcurl' );	
+// 	curl.setHeaders([
+// 			'curl -X GET --header "Accept: application/json" --header "user-key: f7f7a4a4a441facbf11975a77711a113" "https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999"'
+// 	])
+// 		.get('https://developers.zomato.com/api/v2.1/establishments?city_id=83&lat=-33.4454784%20&lon=-70.66091519999999')
+// 			.then(response => response.json())
+// 				.then(data => { 
+// 						console.log(data);
+// 						renderInfo(data);
+// 						})
+
+// 			.catch((e) => {
+// 				console.log(e);
+// 			});
+
+// }	
 
 
 
@@ -88,29 +134,29 @@ function mostrarMapa(){
 			radius: 5000,
 			types: ['restaurant'],
 		};
- 
 		// Creamos el servicio PlaceService y enviamos la petición.
 		let service = new google.maps.places.PlacesService(map);
- 
+			
 		service.nearbySearch(request, function(results, status) {
 			if (status === google.maps.places.PlacesServiceStatus.OK) {
 				for (let i = 0; i < results.length; i++) {
 					crearMarcador(results[i]);
 				}
-			}
+	 			}
+ 		});
+	});
+  function crearMarcador(place) {
+		// Creamos un marcador
+		let marker = new google.maps.Marker({
+			map: map,
+			position: place.geometry.location,
 		});
-	});
- }
- function crearMarcador(place) {
-	// Creamos un marcador
-	let marker = new google.maps.Marker({
-		map: map,
-		position: place.geometry.location,
-	});
-
-// Asignamos el evento click del marcador
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
-		infowindow.open(map, this);
-	});
+ 
+	// Asignamos el evento click del marcador
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.setContent(place.name);
+			infowindow.open(map, this);
+		});
+	
 	}
+}
